c56a601a3bddda309a591e50679c91021dcc2679
camilleowens <camille.owens@tomorrow.io>
2024-12-08 18:55:02 +0530

    Day 08

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,5 +1,7 @@
 use std::error::Error;
 use std::fs;
+use std::fs::File;
+use std::io::BufReader;
 
 pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
 
@@ -9,4 +11,21 @@ pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
         .expect("Should have been able to read the file");
 
     Ok(contents)
+}
+
+pub fn read_file_to_string(file_path: &str) -> Result<String, Box<dyn Error>> {
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    Ok(contents)
+}
+
+pub fn create_buf_reader(file_path: &str) -> Result<BufReader<File>, Box<dyn Error>> {
+    println!("The file you have chosen is: {}", file_path);
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    Ok(reader)
 }
